054d8f6cd53c3e70eec614e5ea91ac897caab371
Ashish Pateliya <pateliyaashish7@gmail.com>
2025-04-01 11:22:18 +0530

    your commit message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  server: {
+    port: 5173, // Set development server port
+  },
+  preview: {
+    port: 5173, // Set preview server port
+  }
 })
