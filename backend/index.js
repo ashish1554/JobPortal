e77ff365d2a9d3469910773768b1f86cb5a3ac9f
Ashish Pateliya <pateliyaashish7@gmail.com>
2025-06-12 23:04:23 +0530

    hii

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,11 @@ const corsOptions = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 };
+app.use(cors({
+  origin: true, // allow all origins
+  credentials: true
+}));
+
 app.options('*', cors(corsOptions));
 
 
