89bd42b2f36db49927f448c8b32691876122d58a
Ashish Pateliya <pateliyaashish7@gmail.com>
2025-06-12 15:18:32 +0530

    first

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser())
 const corsOptions = {
     origin: ['http://localhost:5173', 'https://jp-frontend-cx49.onrender.com'], 
     credentials: true,
-    // methods: "GET,POST,PUT,DELETE",
+    methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
 app.use(cors(corsOptions));
